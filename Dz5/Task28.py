79000ffdbdf7e2e4d71aa5b80901be728337de44
Elza-8 <dulieva-xxl@mail.ru>
2023-05-11 10:51:05 +0500

    added Dz5

---
+++ Dz5/Task28.py
@@ -0,0 +1,26 @@
+# Задача 28: Напишите рекурсивную функцию sum(a, b), возвращающую
+# сумму двух целых неотрицательных чисел. Из всех арифметических
+# операций допускаются только +1 и -1. Также нельзя использовать циклы.
+# return sum(a,b-1) + 1 - Так делать нельзя
+
+# *Пример:*
+
+# 2 2
+#     4
+
+def sum(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    elif a > b:
+        return sum(a - 1, b) + 1
+    else:
+        return sum(a, b - 1) + 1
+
+
+first = int(input('Введите число A: '))
+second = int(input('Введите число B: '))
+
+result = sum(first, second)
+print(f"Сумма чисел {first} и {second} равна {result}")
